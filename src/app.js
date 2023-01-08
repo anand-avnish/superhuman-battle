f4544a07a0e8b1b5166acec039c3b35f28a2bf81
Avnish Anand <62175110+anand-avnish@users.noreply.github.com>
2023-01-08 18:55:31 +0530

    Completed

---
+++ src/app.js
@@ -0,0 +1,92 @@
+const PLAYERS = [
+    "Spiderman",
+    "Captain America",
+    "Wonderwoman",
+    "Popcorn",
+    "Gemwoman",
+    "Bolt",
+    "Antwoman",
+    "Mask",
+    "Tiger",
+    "Captain",
+    "Catwoman",
+    "Fish",
+    "Hulk",
+    "Ninja",
+    "Black Cat",
+    "Volverine",
+    "Thor",
+    "Slayer",
+    "Vader",
+    "Slingo"
+];
+
+// Player Class
+class Player {
+    constructor(id, name, type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.strength = this.getRandomStrength();
+        this.image = "images/super-"+id+".png";
+    }
+
+    // getting random strength
+    getRandomStrength = () => {
+        return Math.ceil(Math.random() * 100);
+    }
+
+    // Create a player for displaying
+    view = () => {
+        let player = document.createElement("div");
+        player.classList.add("player");
+        player.setAttribute("data-id", this.id);
+        
+        player.innerHTML=`
+            <div class="name">${this.name}</div>
+            <img src="${this.image}">
+            <div class="strength">${this.strength}</div>
+        `
+        return player;
+    }
+}
+
+// Superwar Class
+class Superwar {
+    constructor(players) {
+        this.players = players.map((player, i) => {
+            return new Player(i+1, player, (i<players.length/2)? "hero" : "villain");
+        });
+    }
+
+    // Display players in HTML
+    viewPlayers = () => {
+        let team = document.getElementById('heroes');
+        team.innerHTML = '';
+        let fragment =
+            this.buildPlayers('hero');
+        team.append(fragment);
+
+        team = document.getElementById('villains');
+        team.innerHTML = '';
+        fragment =
+            this.buildPlayers('villain');
+        team.append(fragment);
+    }
+
+    // Build players fragment 
+    buildPlayers = (type) => {
+        let fragment = document.createDocumentFragment();
+        this.players
+            .filter(player => player.type == type)
+            .forEach(player => fragment.append(player.view()));
+        return fragment;
+    }
+
+}
+
+
+window.onload = () => {
+    const superwar = new Superwar(PLAYERS);
+    superwar.viewPlayers();
+}
